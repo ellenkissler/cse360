0b801bb09c7ebfb49e387284975daa30a9aadc22
ellenkissler <46464135+ellenkissler@users.noreply.github.com>
2019-02-25 13:48:29 -0700

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -3,24 +3,26 @@
  * Class ID 441
  * Description: This calculator will be able to add, subtract, multiply, 
  * divide, and return the history of all actions completed. 
- * @version 2.0
+ * @version 3.0
  * @date 2/25/19
  */
 package cse360assign2;
 
 public class Calculator {
 
-	private int total;
+	private int total = 0;
+	private String history = "0 ";
 	
 	/**
 	 * A constructor for the calculator class.
 	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history = "0 ";
 	}
 	
 	/**
-	 * This method will return the total that has been calculated by the calculator. 
+	 * This void method will return the total that has been calculated by the calculator. 
 	 * @return	the number after all calculations have been completed
 	 */
 	public int getTotal () {
@@ -28,31 +30,38 @@ public class Calculator {
 	}
 	
 	/** 
-	 * This method will add the given input to the total. 
+	 * This void method will add the given input to the total. 
+	 * It also updates the history string. 
 	 * @param value 	the value to be added to the total
 	 */
 	public void add (int value) {
 		total = total + value;
+		history = history + "+ " + value + " ";
 	}
 	
 	/**
-	 * This method will subtract the given input from the total. 
+	 * This void method will subtract the given input from the total. 
+	 * It also updates the history string. 
 	 * @param value 	the value to be subtracted from the total 
 	 */
 	public void subtract (int value) {
 		total = total - value;
+		history = history + "- " + value + " ";
 	}
 	
 	/**
-	 * This method will multiply the total by the given input. 
+	 * This void method will multiply the total by the given input.
+	 * It also updates the history string. 
 	 * @param value 	the value to multiply the total by
 	 */
 	public void multiply (int value) {
 		total = total * value;
+		history = history + "* " + value + " ";
 	}
 	
 	/**
-	 * This method will divide the total by the given input. 
+	 * This void method will divide the total by the given input. 
+	 * It also updates the history string.
 	 * @param value		the value to divide the total by
 	 */
 	public void divide (int value) {
@@ -62,6 +71,7 @@ public class Calculator {
 		else {		
 			total = total / value; 
 		}
+		history = history + "/ " + value + " ";
 	}
 	
 	/** 
@@ -69,6 +79,6 @@ public class Calculator {
 	 * @return		the history of all calculations performed 
 	 */
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
